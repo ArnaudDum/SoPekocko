f345649a8aa3379feb85776299ded62de706a389
Arnaud Dumoncel <arnaud.dumoncel@gmail.com>
2021-02-21 19:28:33 +0100

    ajout séurité

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const Ddos = require('ddos');
+const ddos = new Ddos({burst: 10, limit: 15});
+const helmet = require('helmet');
+const xssClean = require('xss-clean');
+const session = require('cookie-session');
+const expiryDate = new Date(Date.now() + 60 * 60 * 1000); // Session d'une heure
 const app = express();
+
 
 const Routes = require('./routes/routes.js');
 const UserRoutes = require('./routes/userRoutes.js');
@@ -14,6 +21,23 @@ mongoose.connect('mongodb+srv://DumAr:highjump@piquantecluster.2mtl4.mongodb.net
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
+
+app.use(ddos.express);
+
+app.use(helmet());
+
+app.use(xssClean());
+
+app.use(session({
+  name: '',
+  keys: ['RandomKey1', 'RandomKey2'],
+  cookie: {
+    secure: true,
+    httpOnly: true,
+    path: 'foo/bar',
+    expires: expiryDate
+  }
+}));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
--- backend/controllers/userControls.js
+++ backend/controllers/userControls.js
@@ -2,42 +2,59 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
+function validEmail(value) {
+    if(/^[A-Za-z0-9\.-]+@[a-z]+\.[a-z]{2,4}$/.test(value)) {
+        return true;
+    } else {
+        return false;
+    };
+};
+
+function validPassword(value) {
+    if(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/.test(value)) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+
 
 exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10)
-        .then(hash => {
-            const user = new User({
-                email: req.body.email,
-                password: hash
-            });
-            user.save()
-                .then(() => res.status(201).json({message: 'Nouveau compte enregistré !'}))
-                .catch(error => res.status(400).json({error}))
-        })
-        .catch(error => res.status(500).json({error}));
+    .then(hash => {
+        const user = new User({
+            email: req.body.email,
+            password: hash
+        });
+        user.save()
+            .then(() => res.status(201).json({message: 'Nouveau compte enregistré !'}))
+            .catch(error => res.status(400).json({error}))
+    })
+    .catch(error => res.status(500).json({error}));
 };
 
 exports.login = (req, res, next) => {
     User.findOne({email: req.body.email})
-        .then(user => {
-            if(!user){
-                return res.status(401).json({message: 'Utilisateur non trouvé !'})
-            }
-            bcrypt.compare(req.body.password, user.password)
-                .then(valid => {
-                    if(!valid) {
-                        return res.status(401).json({message: 'Mot de passe incorrect !'})
-                    }
-                    res.status(200).json({
-                        userId: user._id,
-                        token: jwt.sign(
-                            {userId: user._id},
-                            'RANDOM',
-                            {expiresIn: '24h'}
-                        )
-                    });
-                })
-                .catch(error => res.status(500).json({error}));
-        })
-        .catch(error => res.status(500).json({error}));
+            .then(user => {
+                if(!user){
+                    return res.status(401).json({message: 'Utilisateur non trouvé !'})
+                }
+                bcrypt.compare(req.body.password, user.password)
+                    .then(valid => {
+                        if(!valid) {
+                            return res.status(401).json({message: 'Mot de passe incorrect !'})
+                        }
+                        res.status(200).json({
+                            userId: user._id,
+                            token: jwt.sign(
+                                {userId: user._id},
+                                'RANDOM',
+                                {expiresIn: '24h'}
+                            )
+                        });
+                    })
+                    .catch(error => res.status(500).json({error}));
+            })
+            .catch(error => res.status(500).json({error}));
 };
